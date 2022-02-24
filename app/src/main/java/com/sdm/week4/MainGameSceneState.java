4720c09294b86acecbf0616848e75f6f1341efc2
qnise <joeligma317@gmail.com>
2022-02-24 15:01:09 +0800

    commit

--- app/src/main/java/com/sdm/week4/MainGameSceneState.java
+++ app/src/main/java/com/sdm/week4/MainGameSceneState.java
@@ -20,6 +20,7 @@ public class MainGameSceneState implements StateBase {
     public void OnEnter(SurfaceView _view)
     {
         RenderBackground.Create();
+        StarEntity.Create();
         // Example to include another Renderview for Pause Button
     }
 
